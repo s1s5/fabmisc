35578e35253b924ceefc928503917498577f9c23
Shogo Sawai <shogo.sawai@gmail.com>
2016-12-07 19:54:16 +0900

    first commit

---
+++ fabmisc/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+__author__ = 'Shogo Sawai'
+__version__ = '0.0.1'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+from fabmisc import __author__, __version__, __license__
+
+setup(
+    name='fabmisc',
+    version=__version__,
+    description='fabric utilities',
+    license=__license__,
+    author=__author__,
+    keywords='fabric',
+    packages=find_packages(),
+    install_requires=['fabtools'],
+)
